fefcf7cfe0db499008aa3a0984ecb24aaa5a86f7
Sandro Moita <sandromoita@investdigital.com.br>
2018-06-03 15:27:41 -0300

    Projeto Criado

---
+++ src/main/java/com/singularschool/aplicacao/SingularApplication.java
@@ -0,0 +1,12 @@
+package com.singularschool.aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingularApplication.class, args);
+	}
+}
---
+++ src/test/java/com/singularschool/aplicacao/SingularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.singularschool.aplicacao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SingularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
